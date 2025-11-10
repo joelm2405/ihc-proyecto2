24c40b3d330309d76c82eb34e9bc19fbb97cb360
Joel Miguel <joel.miguel@utec.edu.pe>
2025-11-10 12:03:16 -0500

    merge-resuelto

--- Assets/Scripts/EarthquakeHybrid.cs
+++ Assets/Scripts/EarthquakeHybrid.cs
@@ -431,17 +431,3 @@ public class EarthquakeHybrid : MonoBehaviour
 #endif
 }
 
-public class EarthquakeHybrid : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
